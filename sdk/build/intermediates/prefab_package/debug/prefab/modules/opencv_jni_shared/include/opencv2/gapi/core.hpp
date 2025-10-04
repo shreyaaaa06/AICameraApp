a9caa4a844d6ec8cd34328baa942dcf249a7005b
shreyaaaa@06 <sshreyakp@gmail.com>
2025-10-04 10:23:26 +0530

    Remove OpenCV SDK - too large for Git

--- sdk/build/intermediates/prefab_package/debug/prefab/modules/opencv_jni_shared/include/opencv2/gapi/core.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ae27dfb21fee730bee122e5a129aad1fdaad1c6a3b7d00672dce6ba9c2083ad4
-size 80370
--- sdk/native/jni/include/opencv2/gapi/gstreaming.hpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b61fa187d2abaa3f79a93b2ae764eccd84cf090092c0995fbf2514f8372a9429
-size 14940
